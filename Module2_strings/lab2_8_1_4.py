59456027feb2fdeab9eb1bcc5e75a95ebde1e6a1
Richard Lo <90041190+RichardLoProjects@users.noreply.github.com>
2024-10-16 13:48:13 +0100

    2_8_1_4lab

--- Module2_strings/lab2_8_1_4.py
+++ Module2_strings/lab2_8_1_4.py
@@ -1,1 +1,22 @@
+'''Lab for PCAP 2.8.1.4 @ 2024-10-16'''
+def read_int(prompt:str, min_limit:int, max_limit:int) -> int:
+    result = None
+    ass_err_range = f'({min_limit}..{max_limit})'
+    while result is None:
+        try:
+            result = int(input(prompt))
+            assert result in range(min_limit, 1+max_limit)
+        except ValueError:
+            print('Error: wrong input')
+            result = None
+        except AssertionError:
+            print(f'Error: the value is not within the range {ass_err_range}')
+            result = None
+    return result
 
+def main() -> None:
+    v = read_int('Enter a number from -10 to 10: ', -10, 10)
+    print(f'The number is: {v}')
+
+if __name__ == '__main__':
+    main()
